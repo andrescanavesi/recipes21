4d08703102e8dc7eab04eb96705c40cdc4aecfc3
Andres Canavesi <andres.canavesi@gmail.com>
2019-08-08 10:38:37 -0300

    added method to send transactional
    still is smtp is not enabled so let's keep thisin stand by

--- util/sendinblue_helper.js
+++ util/sendinblue_helper.js
@@ -1,25 +1,63 @@
-var SibApiV3Sdk = require("sib-api-v3-sdk");
-var defaultClient = SibApiV3Sdk.ApiClient.instance;
+const SibApiV3Sdk = require("sib-api-v3-sdk");
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
 
-var apiKey = defaultClient.authentications["api-key"];
+const apiKey = defaultClient.authentications["api-key"];
 apiKey.apiKey = process.env.R21_SENDINBLUE_API_KEY_V3;
 
-var apiInstance = new SibApiV3Sdk.EmailCampaignsApi();
-var emailCampaigns = new SibApiV3Sdk.CreateEmailCampaign();
+sendTransacEmail();
 
-emailCampaigns.name = "Campaign sent via the API";
-emailCampaigns.subject = "My subject";
-emailCampaigns.sender = {name: "From name", email: "recipes21.com@gmail.com"};
-emailCampaigns.type = "classic";
-emailCampaigns.htmlContent = "Hello! This is a test from the Sendinblue's API";
-emailCampaigns.recipients = {listIds: [2]};
+//still is not enabled
+function sendTransacEmail() {
+    var apiInstance = new SibApiV3Sdk.SMTPApi();
 
-apiInstance.createEmailCampaign(emailCampaigns).then(
-    function(data) {
-        console.info("API called successfully. Returned data: ");
-        console.info(data);
-    },
-    function(error) {
-        console.error(error);
-    }
-);
+    var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail(); // SendSmtpEmail | Values to send a transactional email
+
+    sendSmtpEmail = {
+        to: [
+            {
+                email: "andres.canavesi@gmail.com",
+                name: "Andres Canavesi",
+            },
+        ],
+        templateId: 8,
+        params: {
+            name: "John",
+            surname: "Doe",
+        },
+        headers: {
+            "X-Mailin-custom": "custom_header_1:custom_value_1|custom_header_2:custom_value_2",
+        },
+    };
+
+    apiInstance.sendTransacEmail(sendSmtpEmail).then(
+        function(data) {
+            console.log("API called successfully. Returned data: " + data);
+        },
+        function(error) {
+            console.error(error);
+        }
+    );
+}
+
+//works
+function sendCampaign() {
+    const apiInstance = new SibApiV3Sdk.EmailCampaignsApi();
+    const emailCampaigns = new SibApiV3Sdk.CreateEmailCampaign();
+
+    emailCampaigns.name = "Campaign sent via the API";
+    emailCampaigns.subject = "My subject";
+    emailCampaigns.sender = {name: "From name", email: "recipes21.com@gmail.com"};
+    emailCampaigns.type = "classic";
+    emailCampaigns.htmlContent = "Hello! This is a test from the Sendinblue's API";
+    emailCampaigns.recipients = {listIds: [2]};
+
+    apiInstance.createEmailCampaign(emailCampaigns).then(
+        function(data) {
+            console.info("API called successfully. Returned data: ");
+            console.info(data);
+        },
+        function(error) {
+            console.error(error);
+        }
+    );
+}
